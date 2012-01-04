0e99c6528124f03e8a263c6eb0e18fb68fb0bdb6
Peter Bay Bastian <peter.bastian@gmail.com>
2012-01-04 17:25:37 +0100

    More work on DiscoveryBroadcaster

--- Core/src/p2pchatroom/core/DiscoveryBroadcaster.java
+++ Core/src/p2pchatroom/core/DiscoveryBroadcaster.java
@@ -11,17 +11,36 @@ public class DiscoveryBroadcaster {
     }
 
     public void broadcast() throws IOException, UnknownHostException {
-        DatagramSocket socket = new DatagramSocket();
-        //InetAddress local = InetAddress.getLocalHost();
-        //NetworkInterface networkInterface = NetworkInterface.getByInetAddress(local);
-        //InetAddress broadcastAddress = networkInterface.getInterfaceAddresses().get(0).getBroadcast();
         byte[] message = "P2PChatRoom".getBytes();
-        DatagramPacket packet = new DatagramPacket(message, message.length, InetAddress.getByName("192.168.137.1"), port);
+        InetAddress group = InetAddress.getByName("224.0.0.1");
+        MulticastSocket socket = new MulticastSocket(port);
+        socket.joinGroup(group);
+        DatagramPacket packet = new DatagramPacket(message, message.length, group, port);
         socket.send(packet);
     }
 
     public static void main(String[] args) throws IOException, SocketException {
-        DiscoveryBroadcaster broadcaster = new DiscoveryBroadcaster(1666);
-        broadcaster.broadcast();
+        new Thread() {
+            public void run() {
+                DiscoveryListener listener = new DiscoveryListener(1667);
+                listener.listen();
+            }
+        }.start();
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                DiscoveryBroadcaster broadcaster = new DiscoveryBroadcaster(1667);
+                try {
+                    broadcaster.broadcast();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        }.start();
+        
     }
 }
