eb9040ab291203b9ad5a37a5c0ec9c285c994ff5
Peter Bay Bastian <peter.bastian@gmail.com>
2012-01-05 15:39:24 +0100

    Changes from Systemic33 when bitbucket was down.

--- Core/src/p2pchatroom/core/DiscoveryListener.java
+++ Core/src/p2pchatroom/core/DiscoveryListener.java
@@ -4,17 +4,22 @@ import java.io.IOException;
 import java.net.*;
 
 public class DiscoveryListener {
-    private DatagramSocket datagramsocket;
+    private MulticastSocket multicastSocket;
     private int server_port;
+    private InetAddress group;
     private String program_name;
+    private boolean listening;
 
-    public DiscoveryListener(int server_port) { //Server listens to UDP port XXXX (over 1024)
+    public DiscoveryListener(String host, int server_port) { //Syntax DiscoveryListener(<BROADCAST_IP>, <PORT>)
         this.server_port = server_port;
         this.program_name = "P2PChatRoom";
         try {
-            datagramsocket = new DatagramSocket(this.server_port);
-        } catch (SocketException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            this.group = InetAddress.getByName(host);
+            multicastSocket = new MulticastSocket(server_port);
+            multicastSocket.joinGroup(group);
+            System.out.println("TTL: "+multicastSocket.getTimeToLive());
+        } catch (Exception e) {
+            System.out.println("Exception occured");
         }
     }
     public void listen() {
@@ -23,18 +28,28 @@ public class DiscoveryListener {
         System.out.println("--Bytes generated");
 
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        
-        while(true) {
+
+        listening = true;
+        while(listening) {
             try {
-                datagramsocket.receive(packet);
+                multicastSocket.receive(packet);
                 System.out.println("--Packet received");
             } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                System.out.println("Exception occured");
             }
             String msg = new String(buffer, 0, packet.getLength());
             System.out.println("MESSAGE BELOW:::");
             System.out.println(packet.getAddress().getHostName() + ": " + msg);
             packet.setLength(buffer.length);
+        }
+    }
+    public void stopListen() {
+        try {
+            listening = false;
+            multicastSocket.leaveGroup(group);
+            multicastSocket.close();
+        } catch (Exception e) {
+            System.out.println("Exception occured");
         }
     }
     public void setPort(int server_port) {
@@ -62,7 +77,7 @@ public class DiscoveryListener {
 //            System.out.println(address.getAddress() + " - " + address.getNetworkPrefixLength());
 //        }
 
-        DiscoveryListener listener = new DiscoveryListener(1666);
+        DiscoveryListener listener = new DiscoveryListener("239.255.255.255", 1666);
         listener.listen();
     }
     //END MAIN PROGRAM-------------
