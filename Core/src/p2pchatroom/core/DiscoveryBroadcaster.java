660e3cc5f1836304ed628ba89e481a2a17b08ba7
Peter Bay Bastian <peter.bastian@gmail.com>
2012-01-05 15:54:35 +0100

    The broadcaster should be working now.

--- Core/src/p2pchatroom/core/DiscoveryBroadcaster.java
+++ Core/src/p2pchatroom/core/DiscoveryBroadcaster.java
@@ -4,25 +4,26 @@ import java.io.IOException;
 import java.net.*;
 
 public class DiscoveryBroadcaster {
+    private String host;
     private int port;
 
-    public DiscoveryBroadcaster(int port) {
+    public DiscoveryBroadcaster(String host, int port) {
+        this.host = host;
         this.port = port;
     }
 
-    public void broadcast() throws IOException, UnknownHostException {
-        byte[] message = "P2PChatRoom".getBytes();
-        InetAddress group = InetAddress.getByName("224.0.0.1");
-        MulticastSocket socket = new MulticastSocket(port);
-        socket.joinGroup(group);
-        DatagramPacket packet = new DatagramPacket(message, message.length, group, port);
+    public void send(String message) throws IOException, UnknownHostException {
+        byte[] buffer = message.getBytes();
+        InetAddress group = InetAddress.getByName(host);
+        DatagramSocket socket = new DatagramSocket(port);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, port);
         socket.send(packet);
     }
 
     public static void main(String[] args) throws IOException, SocketException {
         new Thread() {
             public void run() {
-                DiscoveryListener listener = new DiscoveryListener(1667);
+                DiscoveryListener listener = new DiscoveryListener("239.255.255.255", 1666);
                 listener.listen();
             }
         }.start();
@@ -33,14 +34,13 @@ public class DiscoveryBroadcaster {
                 } catch (InterruptedException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
-                DiscoveryBroadcaster broadcaster = new DiscoveryBroadcaster(1667);
+                DiscoveryBroadcaster broadcaster = new DiscoveryBroadcaster("239.255.255.255", 1666);
                 try {
-                    broadcaster.broadcast();
+                    broadcaster.send("P2PChatRoom");
                 } catch (IOException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
             }
         }.start();
-        
     }
 }
