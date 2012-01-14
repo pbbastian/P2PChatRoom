027b430d960dfba08fcf6e0543adc58a6692e80d
Kristian <kdj.1337@gmail.com>
2012-01-15 00:14:18 +0100

    Improved a lot on the functionality

--- GUI/src/ApplicationGUI.java
+++ GUI/src/ApplicationGUI.java
@@ -1,19 +1,22 @@
 import net.miginfocom.swing.MigLayout;
 import p2pchatroom.core.*;
+import p2pchatroom.core.events.ClientEventListener;
+import p2pchatroom.core.events.ErrorType;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.UnknownHostException;
-import java.security.PublicKey;
+import java.util.ArrayList;
 
-public class ApplicationGUI implements ActionListener{
+public class ApplicationGUI implements ActionListener, ClientEventListener{
     private Client client;
     private String nickname;
+    private ArrayList<Peer> peers;
     
-    private JTextArea chatLog;
-    private JTextArea userList;
+    private ChatLogPanel chatLog;
     private JTextField chatInput;
+    private JList<String> userList;
     private JButton send;
     
     public ApplicationGUI() {
@@ -23,11 +26,15 @@ public class ApplicationGUI implements ActionListener{
         JPanel panel = new JPanel(layout);
         frame.getContentPane().add(panel);
 
-        chatLog= new JTextArea("CHAT LOG PLACEHOLDER");
-        userList = new JTextArea("USERLIST PLACEHOLDER");
+        chatLog= new ChatLogPanel();
+        userList = new JList<String>();
         chatInput = new JTextField("CHAT INPUT");
         send = new JButton("Send");
         send.addActionListener(this);
+
+        userList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        userList.setLayoutOrientation(JList.VERTICAL);
+        userList.setVisibleRowCount(-1);
 
         panel.add(chatLog, "cell 1 0 3 1, grow, push, gapx 8 10, gapy 10 0");
         panel.add(userList, "dock east, gapx 0 10, gapy 10 10, width 90!");
@@ -38,6 +45,7 @@ public class ApplicationGUI implements ActionListener{
         frame.setSize(640, 480);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
 
         this.nickname = JOptionPane.showInputDialog(null, "Nickname");
         ////////////////////////////////////////////////////////////////GUI END
@@ -45,9 +53,82 @@ public class ApplicationGUI implements ActionListener{
         try {
             client = new Client("238.255.255.255", 9010, 9011,this.nickname);
         } catch (UnknownHostException e) {
-            System.out.println("Error occured: e");
+            System.out.println("Error occured: "+e);
+        }
+        this.peers = client.getPeers();
+
+    }
+    private void updateUserList() {
+        DefaultListModel<String> model = new DefaultListModel<String>();
+        for(Peer list : client.getPeers()) {
+            model.addElement("@"+list.getNickname());
+        }
+        userList.setModel(model);
+    }
+
+    private void analyseInput(String textInput) {
+        if(textInput.substring(0,1).equalsIgnoreCase("/") || textInput.substring(0,1).equalsIgnoreCase("@")) {
+            //Checks input, to see if user was trying to type a command
+            if(textInput.equalsIgnoreCase("/help") || textInput.equalsIgnoreCase("/?")
+                    || textInput.equalsIgnoreCase("/commands")) {
+                //Reports back a list of commands available to the user
+                String[] commandList = new String[] {
+                        "message",
+                        "/users",
+                        "@user <message>",
+                        "/nick <nickname>"
+                };
+                String[] descriptionList = new String[] {
+                        "Send message to chat",
+                        "Lists online users",
+                        "Send private message to user",
+                        "Set new nickname"
+                };
+                for (int i = 0; i < commandList.length; i++) {
+                    //TODO: Add the following text output to chatlog.
+                    //System.out.printf("%-25s - %s\n",commandList[i], descriptionList[i]);
+                }
+
+            } else if(textInput.equalsIgnoreCase("/users")) {
+                //Lists users online
+                ArrayList<Peer> userlist = client.getPeers();
+                System.out.println("ONLINE USERS:");
+                for(Peer peer : userlist) {
+                    //TODO: Add the following text output to chatlog.
+                    //System.out.printf("@%-15s %s\n", peer.getNickname(), peer.getAddress().getHostAddress());
+                }
+
+            } else if(textInput.substring(0,1).equalsIgnoreCase("@")) {
+                //Sends a private message
+                String[] stringParts = textInput.split(" ", 2);
+                String user = stringParts[0].replace("@", "");
+                if(user.equalsIgnoreCase(client.getNickname()) || user.equalsIgnoreCase(client.getNickname())) {
+                    JOptionPane.showMessageDialog(null, "You can't send a Private Message to yourself");
+                } else {
+                    //TODO: Add the sent PM to chatlog
+                    String message = stringParts[1];
+                    client.privateMessage(user, message);
+                }
+
+            } else if(textInput.substring(0,5).equalsIgnoreCase("/nick")) {
+                //Sets nickname of client
+                String[] stringParts = textInput.split(" ");
+                String nickname = stringParts[1];
+                client.setNickname(nickname);
+                //TODO: Notify chatlog of name change
+                //System.out.printf("Nickname set to %s\n", nickname);
+
+            } else {
+                //Reached when command was not found
+                //TODO: Notify chatlog of an uknown command
+                //System.out.println("Invalid command, type /help for a list of commands");
+            }
+        } else {
+            //TODO: Add message to chatlog
+            client.message(textInput);
         }
     }
+
     public static void main(String[] args) {
         ApplicationGUI applicationGUI = new ApplicationGUI();
     }
@@ -55,7 +136,42 @@ public class ApplicationGUI implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == send) {
-            
+            if(!chatInput.getText().equals(null)) {
+                analyseInput(chatInput.getText());
+            }
         }
+    }
+
+    @Override
+    public void onMessageReceived(Peer peer, String message) {
+        //TODO: Notify chatlog
+    }
+
+    @Override
+    public void onPrivateMessageReceived(Peer peer, String message) {
+        //TODO: Notify chatlog
+    }
+
+    @Override
+    public void onNicknameChanged(Peer peer, String oldNickname) {
+        //TODO: Notify chatlog
+        updateUserList();
+    }
+
+    @Override
+    public void onErrorOccurred(ErrorType type, String message) {
+        //TODO: Notify chatlog
+    }
+
+    @Override
+    public void onConnectionEstablished(Peer peer) {
+        //TODO: Notify chatlog
+        updateUserList();
+    }
+
+    @Override
+    public void onConnectionClosed(Peer peer) {
+        //TODO: Notify chatlog
+        updateUserList();
     }
 }
